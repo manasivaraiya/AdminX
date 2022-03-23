72ba4ebb05d1abb2a29fa3fe0dc9cd91df4d9242
Jayesh <nirvejayesh@gmail.com>
2022-03-23 13:52:02 +0530

    feat: get command string in post data and use that in subprocess

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from urllib import request
 import subprocess
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return "Hello How are you?"
+
+    if request.method == "POST":
+        command = request.form.get("command")
+        process = subprocess.Popen(
+            # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+            ["powershell.exe", command],
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, err = process.communicate()
+        return {"test.ps1": out.decode("utf-8"), "err": err.decode("utf-8")}
+
+    if request.method == "GET":
+        return "ping pong"
 
 
 if __name__ == "__main__":
-    pscommand = "Write-Host 'Hello Wolrd!'"
-    process = subprocess.Popen(
-        ["powershell.exe", "-NoProfile", "-Command", pscommand], shell=True
-    )
-# app.run(port = 5000)
+    app.run(port=8080, debug=True)
