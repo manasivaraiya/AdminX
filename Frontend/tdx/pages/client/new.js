6d3c2236dd435efcb97d4a7c709f8831e289674b
Manasi Varaiya <m.varaiya@somaiya.edu>
2022-03-24 12:46:57 +0530

    landing page made

--- Frontend/tdx/pages/client/new.js
+++ Frontend/tdx/pages/client/new.js
@@ -14,20 +14,11 @@ export default function Demo() {
   //     termsOfService: false,
   //   },
   // });
-<<<<<<< HEAD
   const [name, setName] = useState('');
   const [url, setUrl] = useState('');
   const [description, setDescription] = useState('');
   const [status, setStatus] = useState('');
   const [lastresponse, setLastResponse] = useState('');
-=======
-  const [name,setName] = useState('');
-  const [url,setUrl] = useState('');
-  const [id,setId] = useState('');
-  const [description,setDescription] = useState('');
-  const [status,setStatus] = useState('');
-  const [lastresponse,setLastResponse] = useState('');
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
 
   const handleOnChange = (e) => {
     setName(e.target.value);
@@ -38,14 +29,9 @@ export default function Demo() {
     setLastResponse(e.target.value);
   }
 
-<<<<<<< HEAD
-  const createUser = () => {
-    const userRef = firebase.database().ref("User")
-=======
   const createUser = (e) =>{
     e.preventDefault();
     // const userRef = firebase.database().ref("User")
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
     const user = {
       name,
       url,
@@ -72,23 +58,14 @@ export default function Demo() {
           label="Name"
           placeholder="xyz"
           onChange={handleOnChange}
-<<<<<<< HEAD
-          value={name}
-          {...form.getInputProps('name')}
-=======
           value = {name}
           // {...form.getInputProps('name')}
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
         />
 
         <TextInput
           label="URL"
           placeholder="iu6urdhcg"
           onChange={handleOnChange}
-<<<<<<< HEAD
-          value={url}
-          {...form.getInputProps('url')}
-=======
           value = {url}
           // {...form.getInputProps('url')}
         />
@@ -99,45 +76,29 @@ export default function Demo() {
           onChange={handleOnChange}
           value = {id}
           // {...form.getInputProps('id')}
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
         />
 
         <TextInput
           label="Description"
           onChange={handleOnChange}
-<<<<<<< HEAD
-          value={description}
-          {...form.getInputProps('description')}
-=======
           value = {description}
           // {...form.getInputProps('description')}
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
         />
 
         <TextInput
           label="Status"
           placeholder="Active"
           onChange={handleOnChange}
-<<<<<<< HEAD
-          value={status}
-          {...form.getInputProps('status')}
-=======
           value = {status}
           // {...form.getInputProps('status')}
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
         />
 
         <TextInput
           label="Last Response"
           placeholder="24/12/21 - 09:30"
           onChange={handleOnChange}
-<<<<<<< HEAD
-          value={lastresponse}
-          {...form.getInputProps('lastResponse')}
-=======
           value = {lastresponse}
           // {...form.getInputProps('lastResponse')}
->>>>>>> 072f8a0c7670120422acab9b97485c7cb879eb5b
         />
 
         <Group position="right" mt="md">
