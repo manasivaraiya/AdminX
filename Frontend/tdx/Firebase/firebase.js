4ccbd661af4d37041ae78fe80ab6b5242d6a062a
Manasi Varaiya <m.varaiya@somaiya.edu>
2022-03-24 15:37:03 +0530

    bg img added

---
+++ Frontend/tdx/Firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDpBi_Ci4Wm0gNPiwgtqkOgtNoUTpcR8E",
+  authDomain: "fir-auth-656f3.firebaseapp.com",
+  projectId: "fir-auth-656f3",
+  storageBucket: "fir-auth-656f3.appspot.com",
+  messagingSenderId: "353735881325",
+  appId: "1:353735881325:web:5ee7497adc863e02d8c564"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export {firebase}
