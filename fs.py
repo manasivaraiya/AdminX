293e1a02b11dcec09094487bb9f5e26cf18334c5
Manan Pandya <mrpandya2017@gmail.com>
2022-03-26 17:05:05 +0530

    Update: UI of Tkinter :(

--- fs.py
+++ fs.py
@@ -1,5 +1,6 @@
 from asyncio import events
 import time
+from turtle import bgcolor, color, width
 from click import command
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -8,7 +9,17 @@ import subprocess
 from threading import Thread, local
 import os
 import requests, json
+# import kivy
+# from kivy.app import App
+# from kivy.uix.dropdown import DropDown
+# from kivy.uix.button import Button
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.label import Label
+# from kivy.core.window import Window
 
+
+
+# kivy.require(kivy.__version__)
 
 OPTIONS = [
     "VLC Media Player",
@@ -21,6 +32,118 @@ OPTIONS = [
     "audacity",
     "Other",
 ]
+
+# class KivyGUIApp(App):
+#     file=''
+#     watchDirectory = "/Users/neelansh/Downloads/"
+
+#     def __init__(self):
+#         self.observer = Observer()
+#         # self.root = KivyGUIApp()
+#         self.file = ""
+
+#     def run(self):
+#         event_handler = Handler()
+#         self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
+#         self.observer.start()
+#         print("observing")
+#         try:
+#             while True:
+#                 time.sleep(5)
+#         except:
+#             self.observer.stop()
+#             print("Observer Stopped")
+#         self.observer.join()
+
+#     def kivy_block(self,file):
+#         self.file=file
+#         dropdown = DropDown()
+#         for opt in OPTIONS :
+#             btn = Button(text ='{}'.format(opt), size_hint_y = None, height = 40,on_press = self.change)
+#             dropdown.add_widget(btn)
+
+#         mainbutton = Button(text ='--- select a file ---', size_hint =(1,None), pos =(0,0 ), width=100, height=60)
+#         mainbutton.bind(on_release = dropdown.open)
+#         dropdown.bind(on_select = lambda instance, x: setattr(mainbutton, 'text', x))
+#         layout = GridLayout(cols=2,rows=1)
+#         layout.add_widget(Label(text="[color=ff0099][b]Please select what kind of file you just downloaded![/b][/color]",markup=True,font_size=20,size_hint_x=1,size_hint_y=None))
+#         layout.add_widget(mainbutton)
+#         return layout
+
+#     def change(self,*args):
+#         if args[0] == "Other":
+#             # self.stop()
+#             print(self.file)
+#             command = f"Remove-Item -Path '{self.file}' "
+#             process = subprocess.Popen(
+#                 # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+#                 ["powershell.exe", command],
+#                 shell=True,
+#                 stdout=subprocess.PIPE,
+#                 stderr=subprocess.PIPE,
+#             )
+#             out, err = process.communicate()
+#             print(out.decode("utf-8"))
+#             print(err.decode("utf-8"))
+
+#         elif args[0] in OPTIONS[:-1]:
+#             print(self.file)
+#             command = f"Get-FileHash -Algorithm SHA256 '{self.file}' | Select Hash | Format-Table -HideTableHeaders "
+#             process = subprocess.Popen(
+#                 # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+#                 ["powershell.exe", command],
+#                 shell=True,
+#                 stdout=subprocess.PIPE,
+#                 stderr=subprocess.PIPE,
+#             )
+#             out, err = process.communicate()
+#             payload = {"name": f"{args[0]}"}
+#             headers = {"Content-Type": "application/json"}
+#             response = requests.post(
+#                 authorized_apps_url, headers=headers, data=json.dumps(payload)
+#             )
+#             print(json.dumps(payload))
+#             print(response.status_code)
+#             if response.status_code == 200:
+#                 hasher = response.json().get("data").get("hash")
+#                 local_hash = out.decode("utf-8")
+#                 escapes = "".join([chr(char) for char in range(1, 32)])
+#                 clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
+#                 clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
+#                 clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
+#                 clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
+#                 if clean_local_hash.lower() != clean_server_hash.lower():
+#                     print('deleting because hash did not match')
+#                     command = f"Remove-Item -Path '{self.file}' "
+#                     process = subprocess.Popen(
+#                         # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+#                         ["powershell.exe", command],
+#                         shell=True,
+#                         stdout=subprocess.PIPE,
+#                         stderr=subprocess.PIPE,
+#                     )
+#             out, err = process.communicate()
+#             print(err.decode("utf-8"))
+#             # self.stop()
+
+#     def wait_till_file_is_created(self, file_path):
+#         file_done = False
+#         file_size = -1
+#         try:
+#             while file_size != os.path.getsize(file_path):
+#                 file_size = os.path.getsize(file_path)
+#                 time.sleep(1)
+#             while not file_done:
+#                 try:
+#                     os.rename(file_path, file_path)
+#                     file_done = True
+#                 except:
+#                     return True
+#         except:
+#             print("ohnoo")
+
+
+
 authorized_apps_url = "https://tdx-nis130.vercel.app/api/authorized_apps"
 
 
@@ -55,15 +178,74 @@ class GUIWithTK:
         label = Label(
             self.root,
             text="Please select what kind of file you just downloaded!",
-            font=("Times New Roman bold", 20),
+            font=("Roboto bold", 20),
         )
+        label.config(bg='black',fg='white')
         label.grid(padx=10, pady=10, sticky="N")
-        self.root.attributes("-fullscreen", True, "-topmost", True)
+        self.root.attributes("-fullscreen", True, "-topmost", True,'-alpha',0.75)
         self.root.configure(bg="black")
         w = OptionMenu(self.root, program, *OPTIONS, command=self.change)
-        w.config(width=20)
+        w.config(width=20,bg='black',fg='white')
         w.grid(column=1, row=10)
+        contents = Frame(self.root)
+        contents.grid(row=1, column=1)
         self.root.mainloop()
+
+    def change(self,*args):
+            if args[0] == "Other":
+                self.root.destroy()
+                print(self.file)
+                command = f"Remove-Item -Path '{self.file}' "
+                process = subprocess.Popen(
+                    # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+                    ["powershell.exe", command],
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+                out, err = process.communicate()
+                print(out.decode("utf-8"))
+                print(err.decode("utf-8"))
+
+            elif args[0] in OPTIONS[:-1]:
+                print(self.file)
+                command = f"Get-FileHash -Algorithm SHA256 '{self.file}' | Select Hash | Format-Table -HideTableHeaders "
+                process = subprocess.Popen(
+                    # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+                    ["powershell.exe", command],
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+                out, err = process.communicate()
+                payload = {"name": f"{args[0]}"}
+                headers = {"Content-Type": "application/json"}
+                response = requests.post(
+                    authorized_apps_url, headers=headers, data=json.dumps(payload)
+                )
+                print(json.dumps(payload))
+                print(response.status_code)
+                if response.status_code == 200:
+                    hasher = response.json().get("data").get("hash")
+                    local_hash = out.decode("utf-8")
+                    escapes = "".join([chr(char) for char in range(1, 32)])
+                    clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
+                    clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
+                    clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
+                    clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
+                    if clean_local_hash.lower() != clean_server_hash.lower():
+                        print('deleting because hash did not match')
+                        command = f"Remove-Item -Path '{self.file}' "
+                        process = subprocess.Popen(
+                            # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
+                            ["powershell.exe", command],
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                        )
+                out, err = process.communicate()
+                print(err.decode("utf-8"))
+                self.root.destroy()
 
     def wait_till_file_is_created(self, file_path):
         file_done = False
@@ -80,63 +262,6 @@ class GUIWithTK:
                     return True
         except:
             print("ohnoo")
-
-    def change(self, *args):
-        if args[0] == "Other":
-            self.root.destroy()
-            print(self.file)
-            command = f"Remove-Item -Path '{self.file}' "
-            process = subprocess.Popen(
-                # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
-                ["powershell.exe", command],
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            out, err = process.communicate()
-            print(out.decode("utf-8"))
-            print(err.decode("utf-8"))
-
-        elif args[0] in OPTIONS[:-1]:
-            print(self.file)
-            command = f"Get-FileHash -Algorithm SHA256 '{self.file}' | Select Hash | Format-Table -HideTableHeaders "
-            process = subprocess.Popen(
-                # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
-                ["powershell.exe", command],
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            out, err = process.communicate()
-            payload = {"name": f"{args[0]}"}
-            headers = {"Content-Type": "application/json"}
-            response = requests.post(
-                authorized_apps_url, headers=headers, data=json.dumps(payload)
-            )
-            print(json.dumps(payload))
-            print(response.status_code)
-            if response.status_code == 200:
-                hasher = response.json().get("data").get("hash")
-                local_hash = out.decode("utf-8")
-                escapes = "".join([chr(char) for char in range(1, 32)])
-                clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
-                clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
-                clean_local_hash = local_hash.translate(str.maketrans("", "", escapes))
-                clean_server_hash = hasher.translate(str.maketrans("", "", escapes))
-                if clean_local_hash.lower() != clean_server_hash.lower():
-                    print('deleting because hash did not match')
-                    command = f"Remove-Item -Path '{self.file}' "
-                    process = subprocess.Popen(
-                        # ["powershell.exe", "D:\\SIH\\Stack-Smashers\\backend\\test.ps1"],
-                        ["powershell.exe", command],
-                        shell=True,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                    )
-            out, err = process.communicate()
-            print(err.decode("utf-8"))
-            self.root.destroy()
-
 
 class Handler(FileSystemEventHandler):
     @staticmethod
@@ -172,5 +297,4 @@ class Handler(FileSystemEventHandler):
 if __name__ == "__main__":
     watch = GUIWithTK()
     watch.run()
-
     # watch.block_with_tkinter('uwu')
